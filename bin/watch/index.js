55064078b136e2b87ddfb980b809862a83284ab9
Kiko Beats <josefrancisco.verdu@gmail.com>
2017-08-18 18:09:35 +0200

    Fix watch on restart

--- bin/watch/index.js
+++ bin/watch/index.js
@@ -9,11 +9,13 @@ const destroySockets = require('./destroy-sockets')
 const getWatchConfig = require('./get-watch-config')
 const restartServer = require('./restart-server')
 
-const logRestart = filename => {
+let isListenRestart = false
+
+const logRestart = ({ filename, forcing }) => {
   const offset = '   '
   const symbol = chalk.blue(logSymbols.info)
   const timestamp = chalk.gray(getTimestamp('HH:mm:ss'))
-  const header = chalk.blue(filename ? 'modified' : 'restart')
+  const header = chalk.blue(forcing ? 'restart' : 'modified')
   const message = chalk.gray(filename || '')
   console.log(`${offset} ${symbol} ${timestamp} ${header} ${message}`)
 }
@@ -22,16 +24,22 @@ module.exports = ({ filename, filepkg, server, cli, sockets }) => {
   const watchConfig = getWatchConfig({ cli, filepkg })
   const watcher = watch('.', watchConfig)
 
-  const doRestart = filename => {
-    logRestart(filename)
+  const doRestart = ({ filename, forcing }) => {
+    logRestart({ filename, forcing })
     destroySockets(sockets)
     server.close(restartServer.bind(this, { filename, filepkg, cli, watcher }))
   }
 
-  watcher.on('all', (event, filename) => doRestart(filename))
+  watcher.on('all', (event, filename) =>
+    doRestart({ filename, forcing: false })
+  )
 
-  process.stdin.on('data', data => {
-    const text = data.toString().trim().toLowerCase()
-    if (text === 'rs') doRestart()
-  })
+  if (!isListenRestart) {
+    isListenRestart = true
+
+    process.stdin.on('data', data => {
+      const text = data.toString().trim().toLowerCase()
+      if (text === 'rs') doRestart({ filename, forcing: true })
+    })
+  }
 }
