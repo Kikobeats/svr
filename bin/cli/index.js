7d1f0dfd90ea7141f2c9415e520490d828e8db1a
Kiko Beats <josefrancisco.verdu@gmail.com>
2017-11-26 12:21:58 +0100

    Update meow interface

--- bin/cli/index.js
+++ bin/cli/index.js
@@ -10,24 +10,32 @@ const getMainFile = require('./get-main-file')
 require('update-notifier')({ pkg }).notify()
 
 const cli = require('meow')(
+  require('fs').readFileSync(path.join(__dirname, 'help.txt'), 'utf8'),
   {
     pkg,
-    help: require('fs').readFileSync(path.join(__dirname, 'help.txt'), 'utf8')
-  },
-  {
-    alias: {
-      p: 'port',
-      L: 'poll',
-      v: 'version',
-      h: 'help',
-      i: 'ignore',
-      H: 'host',
-      c: 'cold',
-      s: 'silent'
-    },
-    default: {
-      poll: false,
-      port: 3000
+    flags: {
+      port: {
+        type: 'number',
+        alias: 'p',
+        default: 3000
+      },
+      poll: {
+        type: 'boolean',
+        alias: 'L',
+        default: false
+      },
+      ignore: {
+        alias: 'i'
+      },
+      host: {
+        alias: 'H'
+      },
+      cold: {
+        alias: 'c'
+      },
+      silent: {
+        alias: 's'
+      }
     }
   }
 )
