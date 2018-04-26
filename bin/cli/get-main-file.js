fbcfbe0f019ccc2451ed3feb50636582fc6ce539
Kiko Beats <josefrancisco.verdu@gmail.com>
2018-04-26 12:05:36 +0200

    Use load-json-file instead of json-future Close #21

--- bin/cli/get-main-file.js
+++ bin/cli/get-main-file.js
@@ -1,10 +1,10 @@
 'use strict'
 
-const jsonFuture = require('json-future')
+const loadJsonFile = require('load-json-file')
 const path = require('path')
 
 module.exports = ({ pwd, input }) => {
-  const pkg = jsonFuture.load(path.resolve(pwd, 'package.json'))
+  const pkg = loadJsonFile.sync(path.resolve(pwd, 'package.json'))
   const { main: mainFile = 'index.js' } = pkg
   const [filename = mainFile] = input
 
