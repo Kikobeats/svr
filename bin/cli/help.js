c8d11de3161c4f63f6b7d5f50f4d60ee3806d998
Kiko Beats <josefrancisco.verdu@gmail.com>
2019-11-30 16:09:16 +0100

    fix: indent

--- bin/cli/help.js
+++ bin/cli/help.js
@@ -10,21 +10,27 @@ module.exports = `
   ${chalk.bold('Options')}
 
     --depth          If set, limits how many levels of subdirectories will be traversed. ${chalk.gray(
-      '[default=undefined]'
-    )}
+    '[default=undefined]'
+  )}
     --help           Show this usage information
-    --host           The host on which svr will run ${chalk.gray("[default='localhost']")}
+    --host           The host on which svr will run ${chalk.gray(
+    "[default='localhost']"
+  )}
     --ignore         Ignore watching a file, directory, or glob ${chalk.gray(
-      '[default=.gitignore, pkg.ignore]'
-    )}
-    --poll           Poll for code changes rather than using events ${chalk.gray('[default=false]')}
+    '[default=.gitignore, pkg.ignore]'
+  )}
+    --poll           Poll for code changes rather than using events ${chalk.gray(
+    '[default=false]'
+  )}
     --port           Port to listen on ${chalk.gray('[default=3000]')}
-    --pwd            A directory to start ${chalk.gray('[default=process.cwd()]')}
+    --pwd            A directory to start ${chalk.gray(
+    '[default=process.cwd()]'
+  )}
     --restart-signal Associate an UNIX signal to be emitted after restart is done
     --version        Output the version number
     --watch          Add more files than the project path to watch ${chalk.gray(
-      '[default=process.cwd()]'
-    )}
+    '[default=process.cwd()]'
+  )}
 
   ${chalk.bold('Examples')}
 
