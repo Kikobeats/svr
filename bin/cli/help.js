63bc2b277389c462ecef103769bc1d85f88fd7e9
Kiko Beats <josefrancisco.verdu@gmail.com>
2019-11-30 15:44:15 +0100

    docs: add --restart-signal

--- bin/cli/help.js
+++ bin/cli/help.js
@@ -9,19 +9,20 @@ module.exports = `
 
   ${chalk.bold('Options')}
 
-    --depth         If set, limits how many levels of subdirectories will be traversed. ${chalk.gray(
+    --depth          If set, limits how many levels of subdirectories will be traversed. ${chalk.gray(
       '[default=undefined]'
     )}
-    --help          Show this usage information
-    --host          The host on which svr will run ${chalk.gray("[default='localhost']")}
-    --ignore        Ignore watching a file, directory, or glob ${chalk.gray(
+    --help           Show this usage information
+    --host           The host on which svr will run ${chalk.gray("[default='localhost']")}
+    --ignore         Ignore watching a file, directory, or glob ${chalk.gray(
       '[default=.gitignore, pkg.ignore]'
     )}
-    --poll          Poll for code changes rather than using events ${chalk.gray('[default=false]')}
-    --port          Port to listen on ${chalk.gray('[default=3000]')}
-    --pwd           A directory to start ${chalk.gray('[default=process.cwd()]')}
-    --version       Output the version number
-    --watch         Add more files than the project path to watch ${chalk.gray(
+    --poll           Poll for code changes rather than using events ${chalk.gray('[default=false]')}
+    --port           Port to listen on ${chalk.gray('[default=3000]')}
+    --pwd            A directory to start ${chalk.gray('[default=process.cwd()]')}
+    --restart-signal Associate an UNIX signal to be emitted after restart is done
+    --version        Output the version number
+    --watch          Add more files than the project path to watch ${chalk.gray(
       '[default=process.cwd()]'
     )}
 
@@ -37,8 +38,11 @@ module.exports = `
     $ svr ${chalk.gray('src/server/routes.js')}
 
   ${chalk.green('–')} Add an external project file to watch
-    $ svr ${chalk.gray('-w node_modules/api-documentation/*.md')}
+    $ svr ${chalk.gray('--watch node_modules/api-documentation/*.md')}
 
   ${chalk.green('–')} Add more project files to be ignored
-    $ svr ${chalk.gray('-i .cache -i public')}
+    $ svr ${chalk.gray('--ignore .cache -i public')}
+
+    ${chalk.green('–')} Emit SIGUSR2 after restart
+    $ svr ${chalk.gray('--restart-signal SIGUSR2')}
 `
